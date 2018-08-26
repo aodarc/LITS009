3d270dfdda52cb4626d276c16afec9b0549ccf09
stupns <stupns@i.ua>
2018-08-26 15:56:15 +0300

    Stupns Homework9

--- homework/Stupns/homework9.py
+++ homework/Stupns/homework9.py
@@ -1,29 +1,38 @@
-import timeit
+import time as t
 
+# start first generator numb
 
-def numberGenerator(n):
-    number = 0
-    while number < n:
-        yield number
-        number += 1
+start = t.time()
 
+for num in range(1, 101):
+    for i in range(2, num):
+        if num % i == 0:
+            break
+    else:
+        print(num, end=' ')
 
-def simple_func():
-    a = []
-    b = a.append(0)
-    c = a.append(1)
-    d = a.append(2)
-    f = a.append(3)
-    g = a.append(4)
-    h = a.append(5)
-    j = a.append(6)
-    k = a.append(8)
-    l = a.append(9)
-    return a
+print('\n')
 
+end = t.time()
+func_time_one = (end - start)
 
-if __name__ == '__main__':
-    print('Work Number generator func ~ms :')
-    print(timeit.timeit("numberGenerator(10)", setup="from __main__ import numberGenerator"))
-    print('Work Simple func ~ms :')
-    print(timeit.timeit("simple_func()", setup="from __main__ import simple_func"))
+# start second simple numb
+
+start = t.time()
+
+spisok = (1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+new_spisok = []
+index = 0
+
+while index < len(spisok):
+    new_spisok.append(spisok)
+    print(new_spisok, '\n')
+    break
+
+end = t.time()
+
+func_time_two = (end - start)
+
+print('Time work generators :', func_time_one, '\n')
+print('Time work cycle while :', func_time_two, '\n')
+print('Time difference :', func_time_two - func_time_one)
